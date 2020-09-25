f1971fb74fa9d7d5ed18a58827e5912046008b5e
tomashilaire <hilairetomas@gmail.com>
2020-09-24 20:27:02 -0700

    Update comprehensions.py
    Agregue unas soluciones que faltaban

--- List_Comp/comprehensions.py
+++ List_Comp/comprehensions.py
@@ -24,7 +24,7 @@ print [n for n in nums]
 #   if n%2 == 0:
 #     my_list.append(n)
 # print my_list
-
+my_list = [n for n in nums if n%2 == 0]
 # Using a filter + lambda
 # my_list = filter(lambda n: n%2 == 0, nums)
 # print my_list
@@ -35,7 +35,7 @@ print [n for n in nums]
 #   for num in range(4):
 #     my_list.append((letter,num))
 # print my_list
-
+my_list = [(letter, num) for letter in 'abcd' for num in range(4)]
 # Dictionary Comprehensions
 names = ['Bruce', 'Clark', 'Peter', 'Logan', 'Wade']
 heros = ['Batman', 'Superman', 'Spiderman', 'Wolverine', 'Deadpool']
@@ -46,7 +46,7 @@ heros = ['Batman', 'Superman', 'Spiderman', 'Wolverine', 'Deadpool']
 # for name, hero in zip(names, heros):
 #     my_dict[name] = hero
 # print my_dict
-
+my_dict = {name: hero for name, hero in zip (names, heros) if name != 'Peter'}
 
 
 # If name not equal to Peter
